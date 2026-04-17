8bd4fafaf4aae149832fa1e2f7c051e193567914
sathappan25 <sp7033@srmist.edu.in>
2026-04-17 21:20:25 +0530

    uc4 completed

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,20 +1,18 @@
-import java.util.List;
-import java.util.LinkedList;
+import java.util.Set;
+import java.util.LinkedHashSet;
 
 public class TrainConsistManagementApp {
     public static void main(String[] args) {
         System.out.println("============================\n");
-        System.out.println("Maintain Ordered Bogie Consist");
+        System.out.println("Preserve Insertion Order of Bogies");
         System.out.println("============================\n");
-        List<String> trainConsist = new LinkedList<>();
-        trainConsist.add("Engine");
-        trainConsist.add("Sleeper");
-        trainConsist.add("AC");
-        trainConsist.add("Cargo");
-        trainConsist.add("Guard");
-        trainConsist.add(2, "Pantry Car");
-        trainConsist.removeFirst();
-        trainConsist.removeLast();
-        System.out.println(trainConsist);
+        Set<String> formation = new LinkedHashSet<>();
+        formation.add("Engine");
+        formation.add("Sleeper");
+        formation.add("Cargo");
+        formation.add("Guard");
+        formation.add("Sleeper");
+        System.out.println("Final Train Formation: \n");
+        System.out.println(formation);
     }
 }
